d9a49612130d3882fe8daf27cba70b1bb8d520fa
makoto860 <93705485+makoto860@users.noreply.github.com>
2022-07-08 18:44:41 +0900

    Create rooms_controller.rb

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,33 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+  
+  def create
+    @room = Room.new(params.require(:room).permit(:room_name, :room_introduction, :price, :address, :room_image))
+      if @room.save
+        flash[:notice] = "ルームを新規登録しました"
+        redirect_to :rooms
+      else
+        render "new"
+      end
+  end
+ 
+  def show
+    @room = Room.find(params[:room_name])
+    
+  end
+ 
+  def edit
+  end
+ 
+  def update
+  end
+ 
+  def destroy
+  end
+end
