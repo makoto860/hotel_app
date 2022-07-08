0aedc0d5fff778d73abc1edbcdc3801b317efb0e
makoto860 <93705485+makoto860@users.noreply.github.com>
2022-07-08 19:50:22 +0900

    Create 20220704085822_create_rooms.rb

---
+++ db/migrate/20220704085822_create_rooms.rb
@@ -0,0 +1,13 @@
+class CreateRooms < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rooms do |t|
+      t.string :room_name
+      t.string :room_introduction
+      t.integer :price
+      t.string :address
+      t.string :room_image
+
+      t.timestamps
+    end
+  end
+end
